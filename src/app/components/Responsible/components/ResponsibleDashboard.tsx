a9541b96ba9a4abb10ea03772cb60e66713c786a
Joao Victor Costa <victor_ngl2@hotmail.com>
2024-01-15 09:52:42 -0300

    [TESTE] Teste do pipeline  # Please enter the commit message for your changes. Lines starting

--- src/app/components/Responsible/components/ResponsibleDashboard.tsx
+++ src/app/components/Responsible/components/ResponsibleDashboard.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 export const ResponsibleDashboard = ({ responsible }: { responsible: any }) => {
     return (
-        <main style={{ backgroundImage: "url('bg.jpg')" }}
+        <main /* style={{ backgroundImage: "url('bg.jpg')" }}*/
             className="h-[100vh] bg-cover">
 
             <div className="h-[10vh] flex justify-between p-4 items-center bg-slate-100">
@@ -9,7 +9,7 @@ export const ResponsibleDashboard = ({ responsible }: { responsible: any }) => {
                     <Image src="/logo.png" width={120} height={120} alt='Cambauba Logo'/>
                 </div>
                 <div className="text-right">
-                    <h1 className="text-sm font-bold text-slate-800">Olá, {responsible.data.attributes.name}</h1>
+                    <h1 className="text-sm font-bold text-slate-800">Olá, Associado(a) {responsible.data.attributes.name}</h1>
 
                 </div>
 
