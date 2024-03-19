621eb0e7f696637c782d28ac66a70ec93703a56b
Joao Victor Costa <victor_ngl2@hotmail.com>
2024-03-19 15:15:24 -0300

    [UPDATE] Add padding to the section icon

--- src/components/Responsible/components/dashboard/SectionIcon.tsx
+++ src/components/Responsible/components/dashboard/SectionIcon.tsx
@@ -6,7 +6,7 @@ export const SectionIcon = ({ icon, title, href }) => {
         <>
             <div className="flex flex-col w-full">
                 <Link href={href}>
-                <div className="flex flex-col items-center justify-center w-full h-32 bg-white rounded-lg shadow-md">
+                <div className="flex flex-col items-center justify-center w-full h-32 bg-white rounded-lg shadow-md p-2">
                     <div className="flex items-center justify-center w-12 h-12 mb-2 rounded-full bg-indigo-100">
                         <Image src={icon} width={24} height={24} alt="attendance" />
                     </div>
